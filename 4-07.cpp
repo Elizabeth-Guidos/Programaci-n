274a4f3cb41dbcde9f8df36d159a644ff06f0b08
Elizabeth-Guidos <136522453+Elizabeth-Guidos@users.noreply.github.com>
2023-07-04 15:14:23 -0600

    Add files via upload

---
+++ 4-07.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+  
+  string nombres[10];
+  string apellidos[10]
+  int m;
+  
+int main(){
+	while (i !=5){
+		cout<<"Elija una opcion"<<endl;
+		cout<<"ingresa un nombre y apellidos"<<endl;
+		cout<<"ingresar las notas de los cursos"<<endl;
+		cout<<"visualizar las notas"<<endl;
+		cout<<"promedio"<<endl;
+		cout<<"salir"<<endl;
+		cin>>i;
+		
+	switch(i){
+		case 1: cout<<"ingrese el nombre y apellido"<<endl;
+		for(int i=0; i<10; i++){
+			cin>>nombre[i];
+			cin>>apellidos[]
+		}
+	}	
+	}
+}
